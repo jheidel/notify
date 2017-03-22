f04cd73507783e0bc415e53aa67c70aecd56815d
Jeff Heidel <jheidel@gmail.com>
2017-03-21 23:36:11 -0700

    Add distutils setup script for lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='jheidel.notify',
+  version='1.0',
+  description='Notify library using irssinotifier',
+  author='Jeff Heidel',
+  author_email='jheidel@gmail.com',
+  url='https://github.com/jheidel/notify',
+  py_modules=['notify'],
+)
