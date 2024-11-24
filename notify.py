8d3bb3ec0011fea3eb7543e5098158a2b05004cf
Jeff Heidel <jheidel@gmail.com>
2024-11-23 18:10:58 -0800

    Rewrite notify library to use google chat webhook.

--- notify.py
+++ notify.py
@@ -2,55 +2,26 @@
 
 import json
 import logging
+import os
 import requests
-import socket
-import string, os, urllib, shlex
-from subprocess import Popen, PIPE
 
-CRED_FILE = '/root/.config/irssi_credentials'
+CONFIG_FILE = '/root/.config/notify.json'
 
 LOG_FILE = '/var/log/notify'
 
-
 def notify(channel, msg, to=None):
-  with open(CRED_FILE) as f:
+  with open(CONFIG_FILE) as f:
     config = json.load(f)
-  creds = config[to] if to is not None else config[config['default_user']]
-  notifier = IrssiNotifier(creds['token'], creds['password'])
-  notifier.send(msg, chan='#%s' % channel, nick=socket.gethostname())
-
-
-class IrssiNotifier(object):
-
-  def __init__(self, api_token, enc_pass):
-    self.api_token = api_token
-    self.enc_pass = enc_pass
-
-  def encrypt(self, text):
-    command = 'openssl enc -aes-128-cbc -salt -base64 -md md5 -A -pass env:OpenSSLEncPW'
-    opensslenv = os.environ.copy()
-    opensslenv['OpenSSLEncPW'] = self.enc_pass
-    output, errors = Popen(
-        shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE,
-        env=opensslenv).communicate(str.encode(text + ' '))
-    output = output.decode('utf-8')
-    output = output.replace('/', '_')
-    output = output.replace('+', '-')
-    output = output.replace('=', '')
-    return output
-
-  def send(self, message, chan='#local', nick='server'):
-    data = {
-      'apiToken': self.api_token,
-      'nick': self.encrypt(nick),
-      'channel': self.encrypt(chan),
-      'message': self.encrypt(message),
-      'version': 24,
-    }
-    url = 'https://irssinotifier.appspot.com/API/Message'
-    logging.info('Now sending message to irssinotifier API.')
-    resp = requests.post(url, data=data)
-    logging.info('Message sent: %s', resp)
+    
+  header = {"title": "[ %s ]" % config["name"], "subtitle": "Channel: %s" % channel}
+  widget = {"textParagraph": {"text": msg}}
+  cards = [
+      {
+          "header": header,
+          "sections": [{"widgets": [widget]}],
+      },
+  ]
+  return requests.post(config["webhook_url"], json={"cards": cards})
 
 
 if __name__ == '__main__':
@@ -71,6 +42,10 @@ if __name__ == '__main__':
     logging.info('Loading message from file %s' % msg)
     with open(msg) as f:
       msg = f.read()
+  TRUNC = 1000
+  msg = msg[:TRUNC]
+  if len(msg) == TRUNC:
+      msg = msg + " ...truncated"
   logging.info('Sending notification to channel "%s":\n%s' % (chan, msg))
   notify(chan, msg)
   sys.exit(0)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
   name='jheidel.notify',
   version='1.0',
-  description='Notify library using irssinotifier',
+  description='Notify library using Google Chat webhook',
   author='Jeff Heidel',
   author_email='jheidel@gmail.com',
   url='https://github.com/jheidel/notify',
